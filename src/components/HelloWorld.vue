5041cc80fd4d1f4071a9cfb5c9dca65645a0ac04
HEXIAYUE <q1506589520q@gmail.com>
2023-08-29 17:54:16 +0800

    '初始化项目'

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,43 @@
+<!--
+ * @Author: HEXIAYUE
+ * @Date: 2023-08-28 00:18:10
+ * @Description: Do not edit
+ * @LastEditors: HEXIAYUE
+ * @LastEditTime: 2023-08-28 01:03:09
+-->
+<script setup lang="ts">
+import { ref } from "vue";
+
+defineProps<{ msg: string }>();
+
+const count = ref(0);
+</script>
+
+<template>
+    <h1>{{ msg }}</h1>
+
+    <div class="card">
+        <button type="button" @click="count += +2">count is {{ count }}</button>
+        <p>
+            Edit
+            <code>components/HelloWorld.vue</code> to test HMR
+        </p>
+    </div>
+    <p>
+        Check out
+        <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank">create-vue</a>, the
+        official Vue + Vite starter
+    </p>
+    <p>
+        Install
+        <a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
+        in your IDE for a better DX
+    </p>
+    <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+</template>
+
+<style scoped>
+.read-the-docs {
+    color: #ab0808;
+}
+</style>
---
+++ src/vite-env.d.ts
@@ -0,0 +1,8 @@
+/*
+ * @Author: HEXIAYUE
+ * @Date: 2023-08-28 00:18:10
+ * @Description: Do not edit
+ * @LastEditors: HEXIAYUE
+ * @LastEditTime: 2023-08-28 00:39:56
+ */
+/// <reference types="vite/client" />
